d703158d91b5224297e06ffa0b92e0fd94fccdbd
kivancekici <kivancekici@gmail.com>
2017-10-29 19:38:54 +0300

    ss

--- js/mainapp.js
+++ js/mainapp.js
@@ -1,9 +1,4 @@
 $(function () {
-    $('input').iCheck({
-        checkboxClass: 'icheckbox_square-blue',
-        radioClass: 'iradio_square-blue',
-        increaseArea: '20%' // optional
-    });
 
 });
 
