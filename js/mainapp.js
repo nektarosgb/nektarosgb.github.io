137229eca3b43e105c1843cff9fd16fd2da1a44d
kivancekici <kivancekici@gmail.com>
2017-10-29 20:57:21 +0300

    sirketbilgilerikaydet

--- js/mainapp.js
+++ js/mainapp.js
@@ -6,11 +6,11 @@ $(function () {
 
 firebase.auth().onAuthStateChanged(function (user) {
     if (user != null) {
-        if (window.location.href.indexOf("index.html")==-1) {
+        if (window.location.href.indexOf("index.html") == -1) {
             window.location = "index.html";
         }
-    }else{
-        if (window.location.href.indexOf("login")==-1) {
+    } else {
+        if (window.location.href.indexOf("login") == -1) {
             window.location = "login.html";
         }
     }
@@ -27,4 +27,42 @@ function checkLogin() {
         });
 }
 
+function kaydetVeritabani(tablo, id, veri) {
+    firebase.database().ref(tablo + '/' + id).set(veri);
+}
+
+function kaydetSirketBilgileri() {
+    var d = new Date();
+    var n = d.getTime()
+
+    var sirketAdi=$("#txtSirketAdi").val();    
+    var idSirket=sirketAdi.replace(/[^\x00-\x7F]/g, "")+n;
+    var sirketAdresi=$("#txtSirketAdresi").val();    
+    var sirketTelefon=$("#txtSirketTelefon").val();   
+    var sirketSGKSicilNo=$("#txtSirketSGKSicilNo").val();   
+    var sirketIlgiliKisi=$("#txtSirketIlgiliKisi").val();   
+    var sirketTelefonCep=$("#txtSirketTelefonCep").val();   
+    var sirketEposta=$("#txtSirketEposta").val();   
+    var sirketIsyeriHekimi=$("#txtSirketIsyeriHekimi").val(); 
+    var sirketIsGuvenligiUzmani=$("#txtSirketIsGuvenligiUzmani").val(); 
+
+    //(Firma Adı, adresi, tel, SGK sicil No, İlgili Kişi, Cep, Email ve İşyeri Hekimi, İş güvenliği Uzmanı)
+
+    var veri={
+        "idSirket":idSirket,
+        "sirketAdi":sirketAdi,
+        "sirketAdresi":sirketAdresi,
+        "sirketTelefon":sirketTelefon,
+        "sirketSGKSicilNo":sirketSGKSicilNo,
+        "sirketIlgiliKisi":sirketIlgiliKisi,
+        "sirketTelefonCep":sirketTelefonCep,
+        "sirketEposta":sirketEposta,
+        "sirketIsyeriHekimi":sirketIsyeriHekimi,
+        "sirketIsGuvenligiUzmani":sirketIsGuvenligiUzmani,
+        "kayitEden":firebase.auth().currentUser.uid
+    }
+
+    kaydetVeritabani("sirketler",idSirket,veri);
+
+}
 
