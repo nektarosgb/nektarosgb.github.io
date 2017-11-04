4cf3254c337ac6db2ef03474c3b835e50d5b2d2b
aydinysf <aydinysf@gmail.com>
2017-11-04 23:47:47 +0300

    dfdf

--- js/pages/modal.js
+++ js/pages/modal.js
@@ -1,4 +1,4 @@
-<script>
+
 var modal = document.getElementById('myModal');
 
 // Get the button that opens the modal
@@ -23,4 +23,3 @@ window.onclick = function(event) {
         modal.style.display = "none";
     }
 }
-</script>
