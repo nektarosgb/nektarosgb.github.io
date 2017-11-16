5b56da4dd91578839ca7458d842e0121b685b998
aydinysf <aydinysf@gmail.com>
2017-11-16 21:44:27 +0300

    dsfd

--- js/mainapp.js
+++ js/mainapp.js
@@ -107,8 +107,8 @@ function kaydetSirketBilgileri() {
     var sirketAdi = $("#txtSirketAdi").val();
     var idSirket = sirketAdi.replace(/[^\x00-\x7F]/g, "") + n;
     console.log($("#hdnId").val());
-    // if($("#hdnId").val()!=='' ||$("#hdnId").val()!==null )
-    //     idSirket=$("#hdnId").val();
+    if($("#hdnId").val()!=='' ||$("#hdnId").val()!==null )
+        idSirket=$("#hdnId").val();
     var sirketAdresi = $("#txtSirketAdresi").val();
     var sirketTelefon = $("#txtSirketTelefon").val();
     var sirketSGKSicilNo = $("#txtSirketSGKSicilNo").val();
