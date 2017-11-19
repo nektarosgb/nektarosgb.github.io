f012a696d35f868125431a04d3fd80c9c0f3ab56
kivancekici <kivancekici@gmail.com>
2017-11-19 14:39:17 +0300

    uyg bug

--- js/pages/tetkik_talep_formu.js
+++ js/pages/tetkik_talep_formu.js
@@ -1,5 +1,5 @@
 function initLoadedPage_tetkik_talep_formu() {
-    LoadDrop('drpUygulamaTuru','idUygulamaTuru','uygulamaTuru','uygulamaturleri','Birimde Yaplacak1511085852689');
+    LoadDrop('drpUygulamaTuru','idUygulamaTuru','uygulamaTuru','uygulama_turleri','Birimde Yaplacak1511085852689');
     LoadDrop('drpCalisan','idCalisan','calisanAdi','calisanlar','0');
     loadCheckBoxList('lstTetkikler', 'idTetkik', 'tetkik', 'tetkikler');
 }
