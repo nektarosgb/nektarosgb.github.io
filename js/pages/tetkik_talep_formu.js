7c572891acb51849011a371e54c6662412aed40f
kivancekici <kivancekici@gmail.com>
2017-11-19 15:25:35 +0300

    secilencalisan

--- js/pages/tetkik_talep_formu.js
+++ js/pages/tetkik_talep_formu.js
@@ -3,7 +3,19 @@ function initLoadedPage_tetkik_talep_formu() {
     LoadDrop('drpUygulamaTuru', 'idUygulamaTuru', 'uygulamaTuru', 'uygulama_turleri', 'Birimde Yaplacak1511085852689');
     LoadDrop('drpCalisan', 'idCalisan', 'calisanAdi', 'calisanlar', '0');
     $("#drpCalisan").change(function () {
-        alert("Secilen:"+this.selectedOptions[0].value);
+        firebase.database().ref('/calisanlar/' + this.selectedOptions[0].value).once('value').then(function (snapshot) {
+
+            $("#hdnIdCalisan").val(snapshot.val().idCalisan);
+            $("#txtCalisanAdi").val(snapshot.val().calisanAdi);
+            $("#txtCalisanAdresi").val(snapshot.val().calisanAdresi);
+            $("#txtCalisanTelefon").val(snapshot.val().calisanTelefon);
+            $("#txtCalisanSGKSicilNo").val(snapshot.val().calisanSGKSicilNo);
+            $("#txtCalisanTCNo").val(snapshot.val().calisanTCNo);
+            $("#txtCalisanTelefonCep").val(snapshot.val().calisanTelefonCep);
+            $("#txtCalisanEposta").val(snapshot.val().calisanEposta);
+            $("#txtCalisanIsyeri").val(snapshot.val().calisanIsyeri);
+            resimGoster("calisanlar", snapshot.val().idCalisan, "imgCalisan");
+        });
     });
 
 }
