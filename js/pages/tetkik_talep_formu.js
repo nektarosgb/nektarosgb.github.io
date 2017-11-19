644d6ebe27d186982fbb2a1010e2b761fda486a1
kivancekici <kivancekici@gmail.com>
2017-11-19 14:53:17 +0300

    title

--- js/pages/tetkik_talep_formu.js
+++ js/pages/tetkik_talep_formu.js
@@ -1,5 +1,6 @@
 function initLoadedPage_tetkik_talep_formu() {
+    loadCheckBoxList('lstTetkikler', 'idTetkik', 'tetkik', 'tetkikler');
     LoadDrop('drpUygulamaTuru','idUygulamaTuru','uygulamaTuru','uygulama_turleri','Birimde Yaplacak1511085852689');
     LoadDrop('drpCalisan','idCalisan','calisanAdi','calisanlar','0');
-    loadCheckBoxList('lstTetkikler', 'idTetkik', 'tetkik', 'tetkikler');
+    
 }
