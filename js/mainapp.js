4a7807477168e6f6271323706e750284d3ab4566
kivancekici <kivancekici@gmail.com>
2017-12-03 16:06:43 +0300

    topars

--- js/mainapp.js
+++ js/mainapp.js
@@ -259,17 +259,12 @@ function validateFields(){
 }
 
 
-//"<button type=\"button\" class=\"btn btn-xs btn-default command-delete\"  data-row-id=\"" + row.idTetkikTalepFormu + "\"><span class=\"fa fa-trash-o\"></span></button>"+
-
 function popoverBtnDeleteCommandHtml(rowid){
-    
-    
     var popoverDeleteCommandHtml="data-trigger=\"focus\"  data-html=\"true\" data-original-title=\"Onaylıyormusunuz ?\" data-container=\"body\" data-toggle=\"popover\" data-placement=\"top\" data-content='<button class=\"btn btn-success btn-block command-delete\" data-row-id=\"" + rowid + "\">Seçili Kaydı Sil</button>'";
     var btnhtml="<button id='btn"+rowid+"' type=\"button\" onclick='showConfirmation(this);' class=\"btn btn-xs btn-default\" "+popoverDeleteCommandHtml+"><span class=\"fa fa-trash-o\"></span></button>";
     return btnhtml;
 }
 function showConfirmation(btn){
-    //$('[data-toggle="popover"]').popover();
     $("#"+btn.id).popover();
 }
 
