0769599e9cb8261382f6c67d6363d343d1f10bbc
kivancekici <kivancekici@gmail.com>
2017-12-03 16:29:13 +0300

    typ

--- js/mainapp.js
+++ js/mainapp.js
@@ -262,7 +262,7 @@ function validateFields(){
 function popoverBtnDeleteCommandHtml(rowid){
 
     //data-container=\"body\" 
-    var popoverDeleteCommandHtml="data-html=\"true\" data-original-title=\"Onaylıyormusunuz ?\" data-toggle=\"popover\" data-placement=\"top\" data-content='<button class=\"btn btn-success btn-block command-delete\" data-row-id=\"" + rowid + "\">Seçili Kaydı Sil</button>'";
+    var popoverDeleteCommandHtml="data-html=\"true\" data-original-title=\"Onaylıyormusunuz ?\" data-toggle=\"popover\" data-placement=\"top\" data-content='<button type='button' class=\"btn btn-success btn-block command-delete\" data-row-id=\"" + rowid + "\">Seçili Kaydı Sil</button>'";
     var btnhtml="<button id='btn"+rowid+"' type=\"button\" onclick='showConfirmation(this);' class=\"btn btn-xs btn-default\" "+popoverDeleteCommandHtml+"><span class=\"fa fa-trash-o\"></span></button>";
     return btnhtml;
 }
