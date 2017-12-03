0383cb6481df1ba5da917185b7123132782a0272
aydinysf <aydinysf@gmail.com>
2017-12-03 13:41:06 +0300

    kıjı

--- js/mainapp.js
+++ js/mainapp.js
@@ -158,7 +158,6 @@ function loadCheckBoxList(lstChkID, idColumn, column, tablo,event) {
 
 function loadPrintTetkikler(){
     firebase.database().ref("tetkikler").once('value').then(function (snapshot) {
-        prntVerileri[tetkikler]={};
         var sayac=0;
         var sayac2=0;
         snapshot.forEach(function (element) {
