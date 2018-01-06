c7ad18148b18659cfd6f4dd5a9104d10d97405e6
kivancekici <kivancekici@gmail.com>
2018-01-06 16:27:42 +0300

    reportt

--- js/reports/mainreport.js
+++ js/reports/mainreport.js
@@ -5,22 +5,47 @@ function initLoadedPage_main_report() {
     $('.datepicker2').datepicker();
 
     $('#btnMainReportYukle').click(function () {
-        var t1 = $('.datepicker1').datepicker('getDate');
-        var t2 = $('.datepicker2').datepicker('getDate');
+        var t1 = $('.datepicker1').datepicker('getDate').getTime();
+        var t2 = $('.datepicker2').datepicker('getDate').getTime();
         loadRptYeniKayitlar(t1, t2);
+        loadRptTetkik(t1, t2);
     });
+}
+
+function loadRptTetkik(t1, t2) {
+    var tetkikSay = 0;
+    firebase.database().ref('tetkiktalepformlari').orderByChild("timestamp").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
+
+
+        tetkikSay += snapshot.seciliTetkikler.length;
+    });
+
+    $("#txtTetkikSayisi").text(tetkikSay);
+
+    var toplamTutar = parseFloat("0.00");;
+    firebase.database().ref('tetkiktalepformlari').orderByChild("timestamp").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
+        snapshot.seciliTetkikler.forEach(idTetkik => {
+            var txtfiyat = chkListVerileri["tetkikler"][idTetkik]["fiyat"];
+            var fiyat = parseFloat(txtfiyat);
+
+            toplamTutar = toplamTutar + fiyat;
+        });
+    });
+
+    $("#txtTetkikToplamTutar").text(toplamTutar);
+
 }
 
 function loadRptYeniKayitlar(t1, t2) {
     var firmasay = 0;
-    firebase.database().ref('sirketler').orderByChild("kayitTarihi").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
+    firebase.database().ref('sirketler').orderByChild("timestamp").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
         firmasay++;
     });
 
     $("#txtSirketSayisi").text(firmasay);
 
     var calisansay = 0;
-    firebase.database().ref('calisanlar').orderByChild("kayitTarihi").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
+    firebase.database().ref('calisanlar').orderByChild("timestamp").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
         calisansay++;
     });
 
@@ -28,7 +53,7 @@ function loadRptYeniKayitlar(t1, t2) {
 
 
     var tetkikFormuSayisi = 0;
-    firebase.database().ref('tetkiktalepformlari').orderByChild("kayitTarihi").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
+    firebase.database().ref('tetkiktalepformlari').orderByChild("timestamp").startAt(t1).endAt(t2).once('value').then(function (snapshot) {
         tetkikFormuSayisi++;
     });
 
